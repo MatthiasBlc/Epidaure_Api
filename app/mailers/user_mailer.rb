92ded71a403e08e4845a0f9c9b34f995aa71c466
enzostk <enzostk@gmail.com>
2022-09-16 16:34:50 +0200

    some tests for reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ApplicationMailer
 
   def welcome_email(user)
-
     @user = user
     @url = 'https://epidaure-preprod.herokuapp.com/'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
-
   end
+
 end
