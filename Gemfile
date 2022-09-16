e0a0f97683d8a30743c86d35e7a5783aeb688af4
enzostk <enzostk@gmail.com>
2022-09-16 11:59:49 +0200

    update for mailer

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
   gem 'letter_opener'
+  gem 'mailjet'
 end
 
 #lazy user creation
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "r202209_BoilerPlateAuth_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'https://epidaure-preprod.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
