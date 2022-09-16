118a53568dd4ce2bad7d43e61394250cfac2a212
MatthiasBlc <102999699+MatthiasBlc@users.noreply.github.com>
2022-09-16 17:01:19 +0200

    Revert "Mailer 02"

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,10 @@
-class UserMailer < Devise::Mailer
-
-  default from :'epidaure.contact@gmail.com'
+class UserMailer < ApplicationMailer
 
   def welcome_email(user)
+
     @user = user
     @url = 'https://epidaure-preprod.herokuapp.com/'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
-  end
 
-  def reset_password_instructions(record, token, opts={})
-    super
   end
-  
 end
