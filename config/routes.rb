86e659950847e2996583572ae198f28cc3e13676
Olivia <oliviarudolf67@gmail.com>
2022-09-22 10:25:16 +0200

    routes mailer contact

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,18 @@ Rails.application.routes.draw do
   resources :time_slots
   resources :practices
   devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get '/member-data', to: 'members#show'
+<<<<<<< HEAD
+  
+  resources :contacts
+  
+=======
+  delete '/members/:id' => 'members#destroy', :as => :admin_destroy_user
+>>>>>>> e8cd763915ae8738f0a6fd9aceffef0ac713c54c
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
